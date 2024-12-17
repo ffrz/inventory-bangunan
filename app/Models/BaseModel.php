ecbb591e2345c2c062cf775fd93d79fa59fa3ff2
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-12-17 08:29:06 +0700

    simplify model

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -10,13 +10,15 @@ use CodeIgniter\Model;
  */
 class BaseModel extends Model
 {
+
+
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = false;
     protected $allowedFields    = true;
 
-    protected bool $allowEmptyInserts = true;
+    protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
 
     protected array $casts = [];
@@ -45,4 +47,19 @@ class BaseModel extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    public function getLastID()
+    {
+        $builder = $this->db->table($this->table);
+        $builder->select($this->primaryKey);
+        $builder->orderBy($this->primaryKey, 'DESC');
+        $builder->limit(1);
+        $query = $builder->get();
+        if ($query->getNumRows() > 0) {
+            $row = $query->getRow();
+            return $row->{$this->primaryKey};
+        } else {
+            return null;
+        }
+    }
 }
